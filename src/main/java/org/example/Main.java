9c8d06b727395a2027a031071b07ed6b6d02b81a
LomTik <vadim.bagy@Mail.ru>
2026-01-07 19:50:49 +0300

    First task completed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,14 @@
 package org.example;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         int menu;
         try (Scanner scanner = new Scanner(System.in)) {
+
+            int[] array = creatingArray(scanner);
             while (true) {
                 System.out.println("Выберите действие: ");
                 System.out.println("0. Выход");
@@ -15,19 +18,20 @@ public class Main {
                 System.out.println("4. Задание 4");
                 System.out.println("5. Задание 5");
 
-                menu = scanner.nextInt();
-                while (!scanner.hasNextInt())
-                {
+
+                while (!scanner.hasNextInt()) {
                     System.out.println("Неверный ввод данных");
                     scanner.next();
                 }
+
+                menu = scanner.nextInt();
 
                 switch (menu) {
                     case 0 -> {
                         return;
                     }
                     case 1 -> {
-                        //ggg
+                        FirstTask(array);
                     }
                     case 2 -> {
 
@@ -50,4 +54,69 @@ public class Main {
     }
 
 
+    //-----Task 1-----
+    public static void FirstTask(int[] array) {
+
+        System.out.print("В прямом порядке: [ ");
+        for (int element: array)
+        {
+            System.out.print(element + " ");
+        }
+        System.out.println("]");
+        System.out.print("В обратном порядке: [");
+        for (int i = array.length-1; i >=0; i--)
+        {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println("]");
+    }
+
+
+    //-----Creating array-----
+    public static int[] creatingArray(Scanner scanner) {
+        Random random = new Random();
+
+        System.out.print("Введите размер массива: ");
+        while (!scanner.hasNextInt()) {
+            System.out.print("Повторите ввод: ");
+            scanner.next();
+        }
+        int[] array = new int[scanner.nextInt()];
+
+
+        System.out.println("Выберите действие: ");
+        System.out.println("1. Ввод массива вручную");
+        System.out.println("2. Случайная генерация массива");
+        while (true) {
+            while (!scanner.hasNextInt()) {
+                System.out.print("Повторите ввод: ");
+                scanner.next();
+            }
+
+            switch (scanner.nextInt()) {
+                case 1 -> {
+                    for(int i = 0; i<array.length; i++)
+                    {
+                        System.out.print("Введите " + (i+1) + " элемент: ");
+                        while (!scanner.hasNextInt()) {
+                            System.out.print("Повторите ввод: ");
+                            scanner.next();
+                        }
+                        array[i] = scanner.nextInt();
+                    }
+                    return array;
+                }
+                case 2 -> {
+                    for (int i = 0; i<array.length; i++)
+                    {
+                        array[i] = random.nextInt(1, 50);
+                    }
+                    return array;
+                }
+                default -> {
+                    System.out.println("Повторите ввод: ");
+                }
+            }
+        }
+    }
 }
