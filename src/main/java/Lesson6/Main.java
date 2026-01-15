d994b45c1c5488f5694342d93c05917276b47b96
LomTik <vadim.bagy@Mail.ru>
2026-01-15 20:20:49 +0300

    Completed lesson 6

--- src/main/java/Lesson6/Main.java
+++ src/main/java/Lesson6/Main.java
@@ -14,7 +14,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)){
-            //firstTask();
+            firstTask();
             secondTask(scanner);
         }
     }
